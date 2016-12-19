4aa8cb0ac1086748fabad31ecd5de735a1390fd6
Aaron Turner <aaron@aaronthedev.com>
2016-12-18 16:05:31 -0800

    Got the project laid out with routing and a home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 
+//No StyleUrls, as the main sass file will handle this
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.scss']
+// })
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'app works!';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
 
+//Our main app component
 import { AppComponent } from './app.component';
+
+//Our Page components
+import { HomeComponent } from './components/home/home.component';
+
+//Declare our routes
+//https://angular.io/docs/ts/latest/guide/router.html
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+  ],
+  exports: [
+      RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
